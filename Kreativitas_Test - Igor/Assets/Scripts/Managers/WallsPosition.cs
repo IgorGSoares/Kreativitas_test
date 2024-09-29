b6b26dfbe77996c8bdfe099087cf0affca9c8a6b
Igor Gilberto Soares <igorgilbertosoares@gmail.com>
2024-09-29 04:35:24 -0300

    Invisible walls auto position

---
+++ Kreativitas_Test - Igor/Assets/Scripts/Managers/WallsPosition.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallsPosition : MonoBehaviour
+{
+    [SerializeField] GameObject leftWall;
+    [SerializeField] GameObject rightWall;
+    void Start()
+    {
+        //Debug.Log(Camera.main.WorldToViewportPoint(rightWall.transform.position));
+        var r = Camera.main.ViewportToWorldPoint(new Vector3(1.05f, 0.63f, 0));
+        var l = Camera.main.ViewportToWorldPoint(new Vector3(-1.05f, 0.63f, 0));
+        rightWall.transform.position = r;
+        leftWall.transform.position = l;
+    }
+}
