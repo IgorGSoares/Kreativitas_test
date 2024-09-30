ddc2de444fc3cfa118e4a94b86904ef71b680062
Igor Gilberto Soares <igorgilbertosoares@gmail.com>
2024-09-30 08:31:36 -0300

    Player move limit

--- Kreativitas_Test - Igor/Assets/Scripts/Player/PlayerMove.cs
+++ Kreativitas_Test - Igor/Assets/Scripts/Player/PlayerMove.cs
@@ -17,6 +17,14 @@ public class PlayerMove : MonoBehaviour
 
         var j = Joystick.Horizontal > 0 ? 1 : Joystick.Horizontal < 0 && Joystick.Horizontal > -1 ? -1 : 0;
         var dir = j * speedMove;
-        transform.position += transform.right * dir * Time.deltaTime;
+        var result = transform.right * dir * Time.deltaTime;
+
+        //Debug.Log(result.x + transform.position.x);
+
+        if(result.x + transform.position.x > Camera.main.ViewportToWorldPoint(new Vector3 (0.85f, 0, 0)).x
+        || result.x + transform.position.x < Camera.main.ViewportToWorldPoint(new Vector3 (0.15f, 0, 0)).x) 
+        return;
+
+        transform.position += result;
     }
 }
